db15e9c7d5c7f7cf161818e16a31f6d2a4697774
Nguyen Huy Hoang <hha3k55@gmail.com>
2023-09-09 11:02:20 +0700

    java

---
+++ Nguyen_Huy_Hoang/JV/buoi1/2.js
@@ -0,0 +1,6 @@
+function myFunction(){
+ var a = parseInt(document.getElementById("num1").value);
+ var b = parseInt(document.getElementById("num2").value);
+ var c = document.getElementById("tong");
+ c.value = a+b;
+}
