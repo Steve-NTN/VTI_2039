21fb38805ba48c457961cf4cff0caef8f1799298
anhnghia47 <trongnghiafes@gmail.com>
2023-09-10 11:53:22 +0700

    remove mp3

--- Nguyen_Trong_Nghia/p2/p2.lession1/index.js
+++ Nguyen_Trong_Nghia/p2/p2.lession1/index.js
@@ -4,9 +4,19 @@ var myclasses = document.getElementsByClassName("myclass");
 // text2.style.color = "#fff";
 // text2.style.color = "#fff";
 
-for (var i = 0; i < myclasses.length; i++) {
-  if (i === 2) {
-    var rdItem = myclasses.item(i);
-    rdItem.style.backgroundColor = "black";
-  }
+// for (var i = 0; i < myclasses.length; i++) {
+//   if (i === 2) {
+//     var rdItem = myclasses.item(i);
+//     rdItem.style.backgroundColor = "black";
+//   }
+// }
+
+
+var mytable = document.getElementById("mytable");
+
+var studentQlt = parseInt(prompt("Quantity?"));
+var name = "Name";
+
+for (var i = 1; i <= studentQlt; i++) {
+  mytable.innerHTML += `<tr><td>${i}</td><td>${name + i}</td></tr>`;
 }
