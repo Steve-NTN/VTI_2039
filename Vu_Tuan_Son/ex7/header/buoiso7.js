f5cab2ad32b247e0243d92cbee92a10ac2cb14dc
Vu_Tuan_Son <vutuanson09@gmail.com>
2023-09-12 19:46:50 +0700

    push my project

---
+++ Vu_Tuan_Son/ex7/header/buoiso7.js
@@ -0,0 +1,7 @@
+$(document).ready(function (param) {
+    $(".box-main").load("buoiso7-banner.html buoiso7-banner.css");
+    //  $(".box-main").load("buoiso7-banner2.html");
+    //   $(".box-main").load("buoiso7-content.html");
+      
+
+  });
