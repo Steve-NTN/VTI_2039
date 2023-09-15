9ef993eb93835c1707e47e86107c8b9c47f869d1
Khanh_Toan <khanhtoan12b2@gmail.com>
2023-09-15 18:44:15 +0700

    push

--- Nguyen_Khanh_Toan/Pack2/JS_Lesson1/index.js
+++
@@ -1,5 +0,0 @@
-console.log('aaa')
-var a= "Hello world"
-var b = 123
-var c= false
-console.log(typeof a,b,c)
---
+++ Nguyen_Khanh_Toan/Pack2/Lesson3/index.js
@@ -0,0 +1,9 @@
+var myInfor = {
+    name: "NKT",
+    age :23,
+};
+
+localStorage.setItem("key",JSON.stringify(myInfor));
+
+document.cookie="cookie"
+    
