afe356f603b5d45964b098683bb20f82dd9b95a0
Thong <khanhthongtran26@gmail.com>
2023-10-08 11:17:11 +0700

    test git

---
+++ Tran_Khanh_Thong/P3/lesson2/Officer.ts
@@ -0,0 +1,14 @@
+class Officer{
+    private name: string;
+    private age : number;
+    private gender : number;
+    private address : string;
+    constructor(name: string, age:number, gender:number, address: string){
+        this.name = name,
+        this.age =age,
+        this.gender=gender,
+        this.address = address
+    }
+
+}
+export {Officer};
---
+++ Tran_Khanh_Thong/P3/lesson2/index.ts
@@ -0,0 +1,4 @@
+
+import { Officer } from './Officer';
+const officer1 = new Officer("A", 23, 0, "hoa")
+const officer2 = new Officer("B", 23, 0, "hong")
